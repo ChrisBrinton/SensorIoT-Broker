c4d74807bdca8dc9d9a881f8cb7e9b0f4688e4c0
Keyvan <keyvanazami@gmail.com>
2025-02-07 22:29:45 -0500

    Dockerizing

--- DataBroker.py
+++ DataBroker.py
@@ -45,9 +45,16 @@ def on_message(client, userdata, msg):
 
 parser = argparse.ArgumentParser(description='DataBroker - MQTT mongodb subscriber for SensorIoT')
 parser.add_argument('--db', help='Database invocation will connect to (default = TEST)', choices=['PROD','TEST'], default='TEST')
+parser.add_argument('--dbconn' , help='Database connect in form <host>:<port>' , default='host.docker.internal')
+parser.add_argument('--host', help='mosquitto listening host' , default = '0.0.0.0')
+parser.add_argument('--port', help='mosquitto listening port' , default = '1883')
 parser.add_argument('--log', help='Log messages to stdout (does not log by default)', action='store_true')
 args = parser.parse_args()
-if (args.db == 'PROD'):
+if (args.dbconn != '' ) :
+  print(args.dbconn)
+  mongoclient = mongodb.MongoClient("mongodb://" + args.dbconn + "/")
+  db = mongoclient.gdtechdb_prod
+elif (args.db == 'PROD'):
   print('PROD db selected')
   mongoclient = mongodb.MongoClient("mongodb://localhost:27017/")
   db = mongoclient.gdtechdb_prod
@@ -66,8 +73,13 @@ mqttclient = mqtt.Client()
 mqttclient.on_connect = on_connect
 mqttclient.on_message = on_message
 
-mqttclient.connect("74.208.159.205", 1883, 60)
-
+#mqttclient.connect("74.208.249.154", 1883, 60)
+host = "127.0.0.1"
+port = "1883"
+if ( args.host != "" ) :
+    host = args.host
+    port = args.port
+mqttclient.connect(host, int(port), 60)
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
     # Other loop*() functions are available that give a threaded interface and a
